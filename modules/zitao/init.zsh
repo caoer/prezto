7ca19489e8b0f47858c64d21e3a1c8ad69bda5d7
Zitao Xiong <caoer115@gmail.com>
2017-01-22 14:37:56 -0500

    personal

---
+++ modules/zitao/init.zsh
@@ -0,0 +1,30 @@
+p() { cd ~/Documents/Projects/$1; }
+_p() { _files -W ~/Documents/Projects -/; }
+compdef _p p
+
+# o() { cd ~/Documents/Projects/OpenSourceProjects/$1; }
+# _o() { _files -W ~/Documents/Projects/OpenSourceProjects -/; }
+# compdef _o o
+
+oo() {
+  `find . \( -iname "*.xcworkspace" ! -iname "project.*" \) -print0 | xargs -0 open`
+  # for f in `find . \( -iname "*.xcworkspace" ! -iname "project.*" \) | cut -c3-`; do
+  #   echo $f
+  #   open $f
+  # done
+}
+_oo() { 
+  _files -W `pwd` -/; 
+}
+compdef _oo oo
+
+function h {
+  cd ~/$1;
+}
+
+function _h {
+  _files -W ~ -/;
+}
+
+compdef _h h
+
